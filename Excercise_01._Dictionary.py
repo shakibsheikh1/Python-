2b1c7dd4bd8a8772375eb0add5ef242a0c4a9baa
shakib sheikh <109619277+shakibsheikh1@users.noreply.github.com>
2022-09-15 14:47:24 +0530

    Add files via upload

---
+++ Excercise_01._Dictionary.py
@@ -0,0 +1,8 @@
+d1 = {"computer":"this is a machime ",
+      "python":"this is a programming language",
+      "c":"this is a basic level program",
+      "software":"this is a related  some program file",
+      "hardware":"this  is a circuitry part"}
+print("entered the key  word in a dictionary...")
+v1 = input()
+print (d1[v1])
